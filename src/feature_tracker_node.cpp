f981dc4ac85d2087f4fed06c5b1c2395247b7436
Goneflash <fz.zhangfan@gmail.com>
2017-10-04 23:48:23 -0700

    Add my_vio code to feature_tracker_node

--- src/feature_tracker_node.cpp
+++ src/feature_tracker_node.cpp
@@ -9,46 +9,63 @@
 
 static const std::string OPENCV_WINDOW = "Image window";
 
-class ImageConverter
-{
+using namespace vio;
+using namespace std;
+
+class FeatureTrackerWrapper {
+ public:
+  FeatureTrackerWrapper() {
+    FeatureMatcherOptions matcher_options;
+    FeatureTrackerOptions tracker_options;
+    if (tracker_options.descriptor_type == "ORB" ||
+        tracker_options.descriptor_type == "FREAK")
+      matcher_options.desc_dist_type = FeatureMatcherOptions::HAMMING;
+    if (tracker_options.descriptor_type == "DAISY" ||
+        tracker_options.descriptor_type == "SIFT" ||
+        tracker_options.descriptor_type == "SURF")
+      matcher_options.desc_dist_type = FeatureMatcherOptions::NORM_L2;
+
+    std::unique_ptr<FeatureMatcher> matcher =
+        FeatureMatcher::CreateFeatureMatcher(matcher_options);
+    tracker_ = FeatureTracker::CreateFeatureTracker(tracker_options,
+                                                    std::move(matcher));
+  }
+
+ private:
+  FeatureTracker *tracker_;
+};
+
+class ImageConverter {
   ros::NodeHandle nh_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
+  FeatureTrackerWrapper tracker_;
 
-public:
-  ImageConverter()
-    : it_(nh_)
-  {
+ public:
+  ImageConverter() : it_(nh_) {
     // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/usb_cam/image_raw", 1,
-      &ImageConverter::imageCb, this);
+    image_sub_ =
+        it_.subscribe("/usb_cam/image_raw", 1, &ImageConverter::imageCb, this);
     image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
     cv::namedWindow(OPENCV_WINDOW);
   }
 
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
+  ~ImageConverter() { cv::destroyWindow(OPENCV_WINDOW); }
 
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
+  void imageCb(const sensor_msgs::ImageConstPtr &msg) {
     cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
+    try {
       cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
+    } catch (cv_bridge::Exception &e) {
       ROS_ERROR("cv_bridge exception: %s", e.what());
       return;
     }
 
     // Draw an example circle on the video stream
     if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255, 0, 0));
 
     // Update GUI Window
     cv::imshow(OPENCV_WINDOW, cv_ptr->image);
@@ -59,10 +76,10 @@ public:
   }
 };
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "image_converter");
   ImageConverter ic;
+
   ros::spin();
   return 0;
 }
