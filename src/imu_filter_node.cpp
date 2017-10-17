20872e639322e43d20f6ad708e347b43bbb9745e
Goneflash <fz.zhangfan@gmail.com>
2017-10-16 22:48:25 -0700

    tilt tracking works

--- src/imu_filter_node.cpp
+++ src/imu_filter_node.cpp
@@ -1,4 +1,5 @@
 #include "my_vio/kalman_filter.hpp"
+#include "my_vio/tilt_tracking.hpp"
 
 #include "ros/ros.h"
 #include "tf/transform_broadcaster.h"
@@ -8,10 +9,13 @@ std::string p_base_stabilized_frame_;
 std::string p_base_frame_;
 tf::TransformBroadcaster *tfB_;
 tf::TransformBroadcaster *tfB_filtered_;
+tf::TransformBroadcaster *tfB_tilt_tracker_;
 tf::StampedTransform transform_;
 tf::StampedTransform transform_filtered_;
+tf::StampedTransform transform_tilt_tracker_;
 tf::Quaternion tmp_;
 IMUKalmanFilter imu_filter;
+vio::TiltTracker<double> tilt_tracker;
 
 bool imu_filter_init;
 
@@ -35,17 +39,25 @@ int main(int argc, char **argv) {
 
   tfB_ = new tf::TransformBroadcaster();
   tfB_filtered_ = new tf::TransformBroadcaster();
+  tfB_tilt_tracker_ = new tf::TransformBroadcaster();
   transform_.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
   transform_filtered_.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
+  transform_tilt_tracker_.setOrigin(tf::Vector3(0.0, 0.0, 0.0));
 
   //	transform_.getOrigin().setX(0.0);
   //	transform_.getOrigin().setY(0.0);
   //	transform_.getOrigin().setZ(0.0);
   transform_.frame_id_ = "/imu_frame";  // p_base_stabilized_frame_;//;
   transform_.child_frame_id_ = "/imu_filtered_frame";  // p_base_frame_;//
+
   transform_filtered_.frame_id_ =
       "/imu_filtered_frame";                     // p_base_stabilized_frame_;//;
   transform_filtered_.child_frame_id_ = "/map";  // p_base_frame_;//
+
+  transform_tilt_tracker_.frame_id_ =
+      "/imu_tilt_tracker_frame";  // p_base_stabilized_frame_;//;
+  transform_tilt_tracker_.child_frame_id_ = "/map";  // p_base_frame_;//
+
   ros::Subscriber imu_subscriber =
       n.subscribe("/imu_raw_data", 5, imuMsgCallback);
 
@@ -54,6 +66,7 @@ int main(int argc, char **argv) {
 
   delete tfB_;
   delete tfB_filtered_;
+  delete tfB_tilt_tracker_;
 
   return 0;
 }
@@ -101,7 +114,6 @@ void imuMsgCallback(const sensor_msgs::Imu &imu_msg) {
   imu_state[3] = imu_msg.angular_velocity.x;
   imu_state[4] = imu_msg.angular_velocity.y;
   imu_state[5] = imu_msg.angular_velocity.z;
-
   if (!imu_filter_init) {
     imu_filter.setInitialState(imu_state);
     imu_filter_init = true;
@@ -120,8 +132,18 @@ void imuMsgCallback(const sensor_msgs::Imu &imu_msg) {
   transform_filtered_.setRotation(tmp_);
   transform_filtered_.stamp_ = ros::Time::now();  // imu_msg.header.stamp;
 
+  double new_roll, new_pitch;
+  tilt_tracker.GetRollPitch(imu_msg.linear_acceleration.x,
+                            imu_msg.linear_acceleration.y,
+                            imu_msg.linear_acceleration.z, new_roll, new_pitch);
+
+  tmp_.setRPY(new_roll, new_pitch, 0);
+  transform_tilt_tracker_.setRotation(tmp_);
+  transform_tilt_tracker_.stamp_ = ros::Time::now();  // imu_msg.header.stamp;
+
   // ROS_INFO("Published filtered roll: %lf, pitch: %lf.", roll, pitch);
 
   tfB_->sendTransform(transform_);
   tfB_filtered_->sendTransform(transform_filtered_);
+  tfB_tilt_tracker_->sendTransform(transform_tilt_tracker_);
 }
